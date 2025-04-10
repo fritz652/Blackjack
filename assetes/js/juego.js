d0cfd15b775a77237f2d188b77001fba9c57a18b
Fritz Guzman <fritz.developer10@gmail.com>
2025-04-10 11:03:34 -0500

    Poker face

---
+++ assetes/js/juego.js
@@ -0,0 +1,16 @@
+let deck = [];
+const tipos = ['C', 'D', 'H', 'S'];
+const especiales = ['A', 'J', 'Q', 'K'];
+
+const crearDeck = () => {
+
+    for (let i=2;i <=10;i++){
+        for (let tipo of tipos){
+            deck.push(i + tipo);
+        }
+    }
+
+    console.log(deck);
+}
+
+crearDeck();
