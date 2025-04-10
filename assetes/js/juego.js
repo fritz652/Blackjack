9bdc4fce5b8f703771c1e33a5e5675225583aebb
Fritz Guzman <fritz.developer10@gmail.com>
2025-04-10 12:08:25 -0500

    POKER FACE 3

--- assetes/js/juego.js
+++ assetes/js/juego.js
@@ -3,7 +3,7 @@ const tipos = ['C', 'D', 'H', 'S'];
 const especiales = ['A', 'J', 'Q', 'K'];
 
 const crearDeck = () => {
-
+//esta funcion crea un nuevo deck de cartas
     for (let i=2;i <=10;i++){
         for (let tipo of tipos){
             deck.push(i + tipo);
@@ -15,9 +15,26 @@ const crearDeck = () => {
             deck.push(esp + tipo);
         }
     }
-    console.log(deck);
+    //console.log(deck);
     deck = _.shuffle(deck); // Barajar el deck usando lodash
     console.log(deck);
 }
 
 crearDeck();
+
+/* :::::::::::REVISAR ESTA SECCION::::::::::::::::: */
+
+// Esta funcion me permite tomar una carta del deck y eliminarla de este
+const pedirCarta = () => {
+    if (deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+    //return deck.pop();
+    let carta = deck.pop(); // Saco una carta del deck
+    
+    console.log(deck);
+    console.log(carta);
+    return carta;
+}
+
+pedirCarta(); // Ejemplo de uso de la funcion pedirCarta
